10645141b46a4fdcc7ad51acf4ab6561220a352b
ИСП-34 <-34@LAB24-14>
2023-10-31 14:33:14 +0300

    Добавьте файлы проекта.

---
+++ Lib_12.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lib_12
+{
+    public class RandomSumCalculator
+    {
+        public string CalculateRandomSum(int n)
+        {
+            Random random = new Random();
+            int sum = 0;
+            string generatedNumbers = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                int randomNumber = random.Next(0, n + 1);
+                sum += randomNumber;
+                generatedNumbers += randomNumber + " ";
+            }
+
+            string result = $"Числа: {generatedNumbers.Trim()}\nСумма: {sum}";
+            return result;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using Lib_12;
+
+namespace Practical1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("1 практическую работу выполнил студент группы ИСП-34 Букин Ярослав.");
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(Input.Text, out int n))
+            {
+                RandomSumCalculator calculator = new RandomSumCalculator();
+                string result = calculator.CalculateRandomSum(n);
+                Result.Text = result;
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите целое число для N");
+            }
+        }
+    }
+}
